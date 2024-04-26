36eed4841042fa452f64f97375b9138da31c6873
yago-codev <yagomilano92@gmail.com>
2024-04-26 10:24:09 -0300

    fazendo scrapper de dados com jsdom

--- src/api/game/services/game.ts
+++ src/api/game/services/game.ts
@@ -2,14 +2,40 @@
  * game service
  */
 import axios from 'axios';
+import { JSDOM } from 'jsdom';
 import { factories } from '@strapi/strapi';
+
+async function getGameInfo(slug) {
+  const gogSlug = slug.replaceAll('-', '_').toLowerCase();
+
+  const body = await axios.get(`https://www.gog.com/game/${gogSlug}`);
+  const dom = new JSDOM(body.data);
+
+  const raw_description = dom.window.document.querySelector( '.description' );
+
+  const description = raw_description.innerHTML;
+  const short_description = raw_description.textContent.slice(0, 160);
+
+  const rating_element = dom.window.document.querySelector(
+    '.age-restrictions__icon use'
+  );
+  console.log(description)
+
+  return {
+    description,
+    short_description,
+    rating: rating_element
+      ? rating_element.getAttribute('xlink:href').replace(/_/g, '').replace('#', '')
+      : 'BR0',
+  }
+}
 
 export default factories.createCoreService('api::game.game', () => ({
   async populate(params) {
-    const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game${params}`;
+    const gogApiUrl = `https://catalog.gog.com/v1/catalog?limit=48&order=desc%3Atrending`;
     const {
       data: { products }
     } = await axios.get(gogApiUrl);
-    console.log(products[0])
+    console.log(await getGameInfo(products[0].slug))
   }
 }));
